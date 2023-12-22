355ec1228472de3cc1b7d25619b68753252098ea
Tyrus Berggren <tyrusbrianberggren@gmail.com>
2023-12-22 15:03:13 -0500

    removed patterns, added dropdown for patterns

--- led-controller-web/src/pages/Controller.jsx
+++ led-controller-web/src/pages/Controller.jsx
@@ -1,10 +1,10 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Heading from "../components/Heading";
-import { useNavigate } from "react-router-dom";
 import { usePattern } from "../context/PatternContext";
 import Options from "../components/Options";
 import patterns from "../data/patterns.json";
-import { HiArrowPath } from "react-icons/hi2";
+import { HiChevronDown } from "react-icons/hi2";
+import { useState } from "react";
 
 const Layout = styled.div`
   display: flex;
@@ -20,11 +20,28 @@ const Layout = styled.div`
   }
 `;
 
-const HeadingBox = styled.div`
+const Header = styled.header`
+  display: flex;
+  justify-content: center;
+`;
+
+const PatternBox = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  gap: 3.6rem;
+  gap: 1.8rem;
+  position: relative;
+`;
+
+const Icon = styled(HiChevronDown)`
+  font-size: 2.4rem;
+  transition: all 0.2s;
+
+  ${(props) =>
+    props.$isOpen &&
+    css`
+      rotate: 180deg;
+    `}
 `;
 
 const Divider = styled.div`
@@ -33,29 +50,96 @@ const Divider = styled.div`
   background-color: var(--color-gray-50);
 `;
 
-const ChangePatternButton = styled.button`
-  line-height: 0;
-  padding: 0.6rem;
-  font-size: 2rem;
-  color: var(--color-gray-50);
+const DropDownWrapper = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  pointer-events: none;
+  padding-top: 1.2rem;
+  z-index: 999;
+
+  ${(props) =>
+    props.$isOpen &&
+    css`
+      pointer-events: all;
+    `}
+
+  @media (max-width: 50em) {
+    position: fixed;
+    padding-top: 0;
+    top: 2.4rem;
+    left: 50%;
+    translate: -50% 0;
+    overflow-y: auto;
+    height: 100%;
+  }
+`;
+
+const DropDown = styled.ul`
+  backdrop-filter: blur(2px);
+  background-color: #404040bb;
+  padding: 1.2rem;
+  border-radius: 0.4rem;
+  border: 1px solid var(--color-gray-600);
+  list-style: none;
+  font: inherit;
+
+  opacity: 0;
+  translate: 0 -1.2rem;
   transition: all 0.2s;
 
-  &:hover {
-    color: var(--color-gray-200);
+  ${(props) =>
+    props.$isOpen &&
+    css`
+      opacity: 1;
+      translate: 0 0;
+    `}
+
+  & li {
+    cursor: pointer;
+    padding: 0.6rem 0;
+    transition: all 0.2s;
+
+    &:hover {
+      color: var(--color-gray-300);
+    }
+
+    &:not(:last-of-type) {
+      border-bottom: 1px solid var(--color-gray-600);
+    }
   }
 `;
 
 function Controller() {
-  const navigate = useNavigate();
-  const { pattern } = usePattern();
+  const { pattern, setPattern } = usePattern();
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <Layout>
-      <HeadingBox>
-        <Heading>{pattern}</Heading>
-        <ChangePatternButton onClick={() => navigate("/pattern")}>
-          <HiArrowPath />
-        </ChangePatternButton>
-      </HeadingBox>
+      <Header>
+        <PatternBox
+          onMouseEnter={() => setIsOpen(true)}
+          onMouseLeave={() => setIsOpen(false)}
+        >
+          <Heading>{pattern} </Heading>
+          <Icon $isOpen={isOpen} />
+          <DropDownWrapper $isOpen={isOpen}>
+            <DropDown $isOpen={isOpen}>
+              {Object.keys(patterns).map((name, i) => (
+                <li
+                  key={name}
+                  onClick={() => {
+                    setPattern(name);
+                    setIsOpen(false);
+                  }}
+                >
+                  {name}
+                </li>
+              ))}
+            </DropDown>
+          </DropDownWrapper>
+        </PatternBox>
+      </Header>
       <Divider />
       <Options options={patterns[pattern]} />
     </Layout>
